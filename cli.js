a9a722e85983293f682671be8226c3637a81783d
Chris Odondi <codondi@ad.unc.edu>
2023-04-28 05:13:27 -0400

    done fr

--- cli.js
+++ cli.js
@@ -1,45 +1,85 @@
 #!/usr/bin/env node
 
-import minimist from 'minimist';
 import moment from 'moment-timezone';
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
+import minimist from 'minimist';
 
-var argv = minimist(process.argv.slice(2));
+// Grab provided args.
+//const [,, ... args] = process.argv
 
-// Help message
-if (argv.h) {
-    console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE\n    -h            Show this help message and exit.\n    -n, -s        Latitude: N positive; S negative.\n    -e, -w        Longitude: E positive; W negative.\n    -z            Time zone: uses tz.guess() from moment-timezone by default.\n    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.\n    -j            Echo pretty JSON from open-meteo API and exit.\n");
-    process.exit(0);
+
+const args  = minimist(process.argv.slice(2));
+//print instructions to console
+if (args.h){
+    let help_text = `Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+    -h            Show this help message and exit.
+    -n, -s        Latitude: N positive; S negative.
+    -e, -w        Longitude: E positive; W negative.
+    -z            Time zone: uses tz.guess() from moment-timezone by default.
+    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+    -j            Echo pretty JSON from open-meteo API and exit.`
+    console.log(help_text);
+    process.exit();
+
+
+    }
+
+//Get CLI Arguments
+var lat=0;
+if (args.n && args.s){console.log('Cannot specify LATITUDE twice')}
+else if (args.n){ lat = Math.round(args.n*100)/100}
+else if (args.s){ lat = -Math.round(args.s*100)/100}
+else{console.log("Latitude must be in range")
+    }
+
+
+var lon=0;
+if (args.w && args.e){console.log('Cannot specify LONGITUDE twice')}
+else if (args.e){lon = Math.round(args.e*100)/100}
+else if (args.w){lon = -Math.round(args.w*100)/100}
+else{console.log('Longitude must be in range')
+ }
+
+ 
+var timezone;
+if (args.z){
+    timezone = args.z;
+} else {timezone = moment.tz.guess()}
+
+
+var days = 1;
+if (args.d!=null) {
+    days = args.d
 }
 
-// Get CLI arguments
-const latitude = argv.n ? Math.round(argv.n * 100) / 100 : argv.s ? -Math.round(argv.s * 100) / 100 : undefined;
-if (latitude == undefined)
-    console.log('Latitude must be in range');
-
-const longitude = argv.e ? Math.round(argv.e * 100) / 100 : argv.w ? -Math.round(argv.w * 100) / 100 : undefined;
-if (longitude == undefined)
-    console.log('Longitude must be in range');
-
-const timezone = argv.z ?? moment.tz.guess();
-
-// Get API Data
-const url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&daily=precipitation_hours&current_weather=true&timezone=" + timezone;
+// Make an API request
+var url = 'https://api.open-meteo.com/v1/forecast?latitude='
++lat
++'&longitude='
++lon
++'&timezone='
++timezone
++'&daily=precipitation_hours';
 const response = await fetch(url);
-const data = await response.json();
 
-// if flag exists, print JSON and exit
-if (argv.j) {
-    console.log(JSON.stringify(data, null, 2));
-    process.exit(0);
+// Get data from API request
+const data = await response.json()
+
+if (args.j){
+    console.log(data)
+    process.exit();
 }
 
-// Response message
-const days = argv.d ?? 1;
-const day_phrase = days == 0 ? "today." : days > 1 ? "in " + days + " days." : "tomorrow.";
+const precipitation_hours = data.daily.precipitation_hours
 
-// Final galosh data
-if (data.daily.precipitation_hours[days] > 0)
-    console.log(`You might need your galoshes ${day_phrase}`);
-else
-    console.log(`You probably won't need your galoshes ${day_phrase}`);
+
+if (days == 0) {
+  console.log("today.")
+} else if (days > 1) {
+  console.log("in " + days + " days.")
+} else {
+  console.log("tomorrow.")
+}
+if (days<0 || days<precipitation_hours.length){
+    console.log(precipitation_hours[days])
+}
